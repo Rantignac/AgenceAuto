87fa753a0a6d9495bceb6018ca412dcb0e111f07
RegisAntignac <antignac.regis@gmail.com>
2018-11-18 16:35:53 +0100

    maj des fixtures de données pour les offres

--- src/DataFixtures/OfferFixture.php
+++ src/DataFixtures/OfferFixture.php
@@ -16,19 +16,17 @@ class OfferFixture extends Fixture
         for ($i = 0; $i < 100; $i++) {
             $offer = new Offer();
             $offer
-                ->setTitle($faker->words(3,true))
-                ->setDescription($faker->sentence(3,true))
-                ->setMiles('20000')
-                ->setColor('rouge')
-                ->setPrice('10000')
+                ->setTitle($faker->words(3, true))
+                ->setDescription($faker->sentence(3, true))
+                ->setMiles($faker->numberBetween(20000, 180000))
+                ->setColor($faker->colorName)
+                ->setPrice($faker->numberBetween(2000, 18000))
                 ->setCity($faker->city)
                 ->setPostalCode($faker->postcode)
-            ->setSold(false);
+                ->setSold(false);
             $manager->persist($offer);
-
 
         }
         $manager->flush();
-
     }
 }
